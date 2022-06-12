b4889ad610bc60773c8113e28b94421966928135
malken21 <Malken_Marumasa@outlook.jp>
2022-06-13 00:32:45 +0900

    Add files via upload

--- java/marumasa/clock/timer.java
+++ java/marumasa/clock/timer.java
@@ -7,15 +7,28 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.Calendar;
 
 public class timer extends BukkitRunnable {
+
+    boolean clock = true;
+
     @Override
     public void run() {
         Calendar calendar = Calendar.getInstance();
-        int MINUTE = calendar.get(Calendar.MINUTE);
-        if (MINUTE == 0) {
-            int YEAR = calendar.get(Calendar.YEAR);
-            int MONDAY = calendar.get(Calendar.MONDAY) + 1;
-            int DAY = calendar.get(Calendar.DATE);
-            int HOUR = calendar.get(Calendar.HOUR_OF_DAY);
+        final int MINUTE = calendar.get(Calendar.MINUTE);
+
+        final int MINUTE_SET = 0;
+
+        if (MINUTE == MINUTE_SET + 1 && !clock) {
+            clock = true;
+            return;
+        }
+
+        if (MINUTE == MINUTE_SET && clock) {
+            clock = false;
+
+            final int YEAR = calendar.get(Calendar.YEAR);
+            final int MONDAY = calendar.get(Calendar.MONDAY) + 1;
+            final int DAY = calendar.get(Calendar.DATE);
+            final int HOUR = calendar.get(Calendar.HOUR_OF_DAY);
 
             String text = String.format("時報 %d/%d/%d/%d:00 になりました", YEAR, MONDAY, DAY, HOUR);
             Bukkit.broadcastMessage(ChatColor.YELLOW + text);
